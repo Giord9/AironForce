a9fcbe5d763854d301b6f34320f8e77ec834a283
Giord9 <giordana.bonavolonta@gmail.com>
2025-07-30 16:18:46 +0000

    Updates

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/servizi/<servizio>')
+def servizio(servizio):
+    nomi_servizi = {
+        "1to1": "Allenamento Personal 1to1",
+        "coppia": "Allenamento Personal di Coppia",
+        "funzionale": "Allenamento Funzionale"
+    }
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    slot_settimanali = {
+        "1to1": {
+            "Lunedì": ["10:00", "11:00", "16:00", "17:00"],
+            "Mercoledì": ["10:00", "11:00", "16:00", "17:00"],
+            "Venerdì": ["10:00", "11:00", "16:00", "17:00"],
+            "Sabato": ["10:00", "11:00", "16:00", "17:00"],
+        },
+        "coppia": {
+            "Martedì": ["18:00", "19:00"],
+            "Giovedì": ["18:00", "19:00"],
+        },
+        "funzionale": {
+            "Lunedì": ["19:00"],
+            "Mercoledì": ["20:00"],
+        }
+    }
+
+    nome = nomi_servizi.get(servizio, "Servizio")
+    slot = slot_settimanali.get(servizio, {})
+
+    return render_template("slot.html", nome=nome, slot_settimanali=slot)
